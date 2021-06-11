23b9d0f15671909e09c11bb73ec4110c6bfe8935
Sirui Mu <msrlancern@126.com>
2021-06-11 11:20:46 +0800

    Scaffold Intel PT kernel module

---
+++ kernel/ipt-info.h
@@ -0,0 +1,9 @@
+#ifndef IPT_INFO_H
+#define IPT_INFO_H
+
+#define IPT_LICENSE     "Dual MIT/GPL"
+#define IPT_AUTHOR      "Sirui Mu"
+#define IPT_DESCRIPTION "Accessing Intel-PT related functionalities"
+#define IPT_VERSION     "0.1"
+
+#endif // IPT_INFO_H
---
+++ kernel/ipt.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#include "ipt-info.h"
+
+MODULE_LICENSE(IPT_LICENSE);
+MODULE_AUTHOR(IPT_AUTHOR);
+MODULE_DESCRIPTION(IPT_DESCRIPTION);
+MODULE_VERSION(IPT_VERSION);
+
+static int __init ipt_init(void) {
+  printk(KERN_INFO "ipt module loaded\n");
+  return 0;
+}
+
+static void __exit ipt_exit(void) {
+  printk(KERN_INFO "ipt module unloaded\n");
+}
+
+module_init(ipt_init);
+module_exit(ipt_exit);
